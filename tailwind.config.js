6a986964f1a8acd816d68e005dc1566c66687cdc
hacker.095 <animelovera1435@gmail.com>
2026-05-17 17:35:49 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1fd361",
+        secondary: "#16a34a",
+      },
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: "1rem",
+          sm: "3rem",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
